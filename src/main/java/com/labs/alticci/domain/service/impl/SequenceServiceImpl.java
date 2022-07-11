f389c7ac8acce191b5008b17a7d9a696b8f23755
Flávio M Barbosa <flavio.barbosa@dataprev.gov.br>
2022-07-11 16:29:53 -0300

    add cache to the service

--- src/main/java/com/labs/alticci/domain/service/impl/SequenceServiceImpl.java
+++ src/main/java/com/labs/alticci/domain/service/impl/SequenceServiceImpl.java
@@ -4,23 +4,38 @@ import com.labs.alticci.api.model.Response;
 import com.labs.alticci.domain.service.SequenceService;
 import org.springframework.stereotype.Service;
 
+import java.util.TreeMap;
+
 @Service
 public class SequenceServiceImpl implements SequenceService {
 
+    private static TreeMap<Integer, Long> cache = new TreeMap<>();
+
+    public SequenceServiceImpl() {
+        cache.put(0, 0L);
+        cache.put(1, 1L);
+        cache.put(2, 1L);
+    }
+
     @Override
     public Response getSequenceValue(Integer index) {
-        Long sequenceValue = calculate(index);
+        Long sequenceValue = cache.containsKey(index) ? cache.get(index) : calculate(index);
+
+        cache.putIfAbsent(index, sequenceValue);
 
         return Response.builder().data(sequenceValue).build();
     }
 
     private Long calculate(Integer index) {
-        if(index == 0)
-            return 0L;
+        if(cache.containsKey(index))
+            return cache.get(index);
 
-        if(index == 1 || index == 2)
-            return 1L;
+        Long calculateMinus3 = calculate(index - 3);
+        cache.putIfAbsent(index - 3, calculateMinus3);
 
-        return calculate(index - 3) + calculate(index - 2);
+        Long calculateMinus2 = calculate(index - 2);
+        cache.putIfAbsent(index - 2, calculateMinus2);
+
+        return calculateMinus3 + calculateMinus2;
     }
 }
